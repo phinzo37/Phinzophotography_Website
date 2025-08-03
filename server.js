6566cf4ac3a06d92bdf0ca84f9b7a607a7d14c59
Anil Gurung <anilgurung@anils-mbp.mynetworksettings.com>
2025-08-03 15:34:27 -0400

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
+import express from 'express';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
 
 const app = express();
 const PORT = 3001;
